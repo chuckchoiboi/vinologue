5eb5dd20449b20f60ee73733820b1419737f4a37
Chuck Choi <hyunchoi94@gmail.com>
2023-11-09 00:39:48 -0800

    add states to filters

--- lib/screens/listing_screen.dart
+++ lib/screens/listing_screen.dart
@@ -1,8 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:vinologue/widgets/listing/listing_filter.dart';
 
-class ListingScreen extends StatelessWidget {
+const listingView = ['stacked', 'row'];
+const filter = ['date', 'rating', 'varietal'];
+
+class ListingScreen extends StatefulWidget {
   const ListingScreen({super.key});
+
+  @override
+  State<ListingScreen> createState() => _ListingScreenState();
+}
+
+class _ListingScreenState extends State<ListingScreen> {
+  String _listingView = listingView[0];
+  String _listingFilter = filter[0];
+
+  void _toggleListingView() {
+    setState(() {
+      _listingView =
+          _listingView == listingView[0] ? listingView[1] : listingView[0];
+    });
+  }
+
+  void _changeListingFilter(int idx) {
+    setState(() {
+      _listingFilter = filter[idx];
+    });
+  }
 
   @override
   Widget build(context) {
@@ -10,14 +34,19 @@ class ListingScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Vinologue'),
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            SizedBox(height: 30),
-            ListingFilter(),
-            SizedBox(height: 30),
-            Text('listing'),
+            const SizedBox(height: 30),
+            ListingFilter(
+              listingView: _listingView,
+              onListingViewToggle: _toggleListingView,
+              listingFilter: _listingFilter,
+              onListingFilterChange: _changeListingFilter,
+            ),
+            const SizedBox(height: 30),
+            const Text('listing'),
           ],
         ),
       ),
--- lib/widgets/listing/listing_filter.dart
+++ lib/widgets/listing/listing_filter.dart
@@ -1,34 +1,70 @@
 import 'package:flutter/material.dart';
 
-class ListingFilter extends StatefulWidget {
-  const ListingFilter({super.key});
+const List<Widget> icons = <Widget>[
+  Text('Date'),
+  Text('Rating'),
+  Text('Varietal')
+];
 
-  @override
-  State<ListingFilter> createState() => _ListingFilterState();
-}
+Map<String, int> filterMap = {
+  'date': 0,
+  'rating': 1,
+  'varietal': 2,
+};
 
-class _ListingFilterState extends State<ListingFilter> {
-  bool isToggleOn = true;
+class ListingFilter extends StatelessWidget {
+  const ListingFilter({
+    super.key,
+    required this.listingView,
+    required this.onListingViewToggle,
+    required this.listingFilter,
+    required this.onListingFilterChange,
+  });
+
+  final String listingView;
+  final void Function() onListingViewToggle;
+  final String listingFilter;
+  final void Function(int) onListingFilterChange;
 
   @override
   Widget build(context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Row(
-          children: [
-            Switch(
-                onChanged: (bool value) {
-                  setState(() {
-                    isToggleOn = value;
-                  });
-                },
-                value: isToggleOn),
-            Text(isToggleOn ? 'Stacked' : 'Row'),
-          ],
-        ),
-        const Text('filter'),
-      ],
+    bool isToggleOn = listingView == 'stacked';
+    int filterIdx = filterMap[listingFilter]!;
+    List<bool> selectedFilter = List.generate(3, (index) => index == filterIdx);
+
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Switch(
+                  onChanged: (bool value) {
+                    onListingViewToggle();
+                  },
+                  value: isToggleOn),
+              const SizedBox(width: 5),
+              Text(isToggleOn ? 'Stacked' : 'Row'),
+            ],
+          ),
+          ToggleButtons(
+            onPressed: (int index) {
+              onListingFilterChange(index);
+              for (int i = 0; i < selectedFilter.length; i++) {
+                selectedFilter[i] = i == index;
+              }
+            },
+            borderRadius: const BorderRadius.all(Radius.circular(20)),
+            isSelected: selectedFilter,
+            constraints: const BoxConstraints(
+              minHeight: 35.0,
+              minWidth: 65.0,
+            ),
+            children: icons,
+          ),
+        ],
+      ),
     );
   }
 }
