2a2049f874a76c4770902dc226e0ca39c3b3d01b
Chuck Choi <hyunchoi94@gmail.com>
2023-11-30 14:12:21 -0800

    update add entry option to top appbar

--- lib/screens/listing_screen.dart
+++ lib/screens/listing_screen.dart
@@ -2,11 +2,59 @@ import 'package:flutter/material.dart';
 
 import 'package:vinologue/widgets/listing/entries_listing.dart';
 import 'package:vinologue/widgets/listing/listing_filter.dart';
+
 import 'package:vinologue/data/entries_data.dart';
 import 'package:vinologue/models/vinologue_entry.dart';
 
 const listingView = ['stacked', 'row'];
 const filter = ['date', 'rating', 'varietal'];
+
+void showNewEntryBottomSheet(BuildContext context) {
+  showModalBottomSheet<void>(
+    context: context,
+    builder: (context) {
+      return SizedBox(
+        height: 200,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text(
+                'Create a new entry',
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+              const SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  OutlinedButton.icon(
+                    icon: const Icon(Icons.visibility_off),
+                    label: const Text('Blind taste'),
+                    style: const ButtonStyle().copyWith(
+                      minimumSize: MaterialStateProperty.all(const Size(
+                          180, 40)), // Adjust the width and height as needed
+                    ),
+                    onPressed: () => Navigator.pop(context),
+                  ),
+                  OutlinedButton.icon(
+                    icon: const Icon(Icons.center_focus_weak),
+                    label: const Text('Scan the bottle'),
+                    style: const ButtonStyle().copyWith(
+                      minimumSize: MaterialStateProperty.all(const Size(
+                          180, 40)), // Adjust the width and height as needed
+                    ),
+                    onPressed: () => Navigator.pop(context),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      );
+    },
+  );
+}
 
 class ListingScreen extends StatefulWidget {
   const ListingScreen({super.key});
@@ -55,6 +103,15 @@ class _ListingScreenState extends State<ListingScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Vinologue'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            tooltip: 'Add a new entry',
+            onPressed: () {
+              showNewEntryBottomSheet(context);
+            },
+          ),
+        ],
       ),
       body: Center(
         child: Column(
@@ -72,13 +129,6 @@ class _ListingScreenState extends State<ListingScreen> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-      floatingActionButtonLocation:
-          FloatingActionButtonLocation.miniCenterFloat,
     );
   }
 }
