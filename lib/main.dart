39dd67b8f90f135fa1bce753b5d9924bf3ddc4a5
Chuck Choi <hyunchoi94@gmail.com>
2023-12-01 14:48:58 -0800

    add firebase to codebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
-import 'package:vinologue/screens/listing_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+// import 'package:vinologue/screens/listing_screen.dart';
 import 'package:vinologue/screens/auth_screen.dart';
 
 var kColorScheme = ColorScheme.fromSeed(seedColor: Colors.deepPurple);
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
